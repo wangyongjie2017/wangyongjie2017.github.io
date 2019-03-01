b30c900ec8f11304b79c858a99c72116197d05e4
王永杰GithubPro <17633559293@163.com>
2019-03-01 17:03:47 +0800

    Site updated: 2019-03-01 17:03:47

---
+++ js/message.js
@@ -0,0 +1,11 @@
+new Valine({
+    el: '#vcomments',
+    appId: 'kUeSMaqt5bTNbbEJfWcRK0nU-gzGzoHsz',
+    appKey: '4T2482n3l2F94os2LSrdDGGV',
+    notify: true,   //邮件提醒
+    verify: true,  //验证码服务
+    avatar: 'mm',
+    placeholder: '说点什么吧...',    //占位符
+    pageSize: 1   //评论分页
+    
+})
