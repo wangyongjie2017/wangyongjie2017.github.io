9d2f7959f674a533825bb275f9b8ca00a7b2ee2c
王永杰GithubPro <17633559293@163.com>
2019-03-01 17:53:32 +0800

    Site updated: 2019-03-01 17:53:32

--- js/app.js
+++ js/app.js
@@ -115,16 +115,14 @@ window.onload = function() {
 
 };
 
-
-// 
+// 留言插件自定义
 new Valine({
-    el: '#vcomments',
+    el: '#vcomments-message',
     appId: 'kUeSMaqt5bTNbbEJfWcRK0nU-gzGzoHsz',
     appKey: '4T2482n3l2F94os2LSrdDGGV',
     notify: true,   //邮件提醒
     verify: true,  //验证码服务
-    avatar: 'mm',
-    placeholder: '说点什么吧...',    //占位符
-    pageSize: 1   //评论分页
+    avatar: 'monsterid',
+    placeholder: '说点什么吧...'    //占位符
     
 })
